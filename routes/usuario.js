c571ab4118ce61553e6c6ce6048e8e31e7325903
jose <josejab95@gmail.com>
2023-01-19 22:05:16 +0100

    cambia las cors de nuevo

--- routes/usuario.js
+++ routes/usuario.js
@@ -1,13 +1,6 @@
 const express = require("express");
 const router = express.Router();
 const userManager = require("../public/javascript/registroManager");
-const cors = require("cors")
-
-var corsOptions = {
-    origin: 'https://twitter-clone-back-production.up.railway.app',
-    optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
-}
-
 
 router.get("/get", async (req, res) => {
 
@@ -16,9 +9,7 @@ router.get("/get", async (req, res) => {
 })
 
 
-router.options('/add', cors(corsOptions))
-
-router.post("/add", cors(corsOptions), async (req, res, next) => {
+router.post("/add", async (req, res, next) => {
 
 
     let usuario = await userManager.createUser(req.body.usuario, req.body.pass);

fa8ce4b8568cad820a5d88c8fc85b7fdb59424f8
jose <josejab95@gmail.com>
2023-01-19 21:27:39 +0100

    añade opciones al servidor

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ const app = express();
 const PORT = 3000;
 const appDatabase = require("./public/javascript/appDatabase");
 const http = require("https")
+const fs = require("fs")
+
 appDatabase.initDatabase();
 
 
@@ -12,9 +14,18 @@ appDatabase.initDatabase();
     //el await aqui no se si haria falta
     await require("./public/javascript/appInit")(express, app);
 
+    const options =
+    {
+        key: fs.readFileSync("certificados/private.pem", "utf8"),
+        cert: fs.readFileSync("certificados/myCA.pem", "utf8"),
+        requestCert: false,
+        rejectUnauthorized: false,
+        secureOptions: require("constants").SSL_OP_NO_SSLv3 | require("constants").SSL_OP_NO_TLSv1,
+        honorCipherOrder: true
+    }
 
 
-    await http.createServer(app).listen(PORT, () => {
+    await http.createServer(app, options).listen(PORT, () => {
         console.log("listening ", process.env.PORT)
     })
 

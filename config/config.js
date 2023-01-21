52114f7a8096bb44af8fec3aa9b23b6c76657d1d
jose <josejab95@gmail.com>
2023-01-21 14:14:34 +0100

    distingue entre produccion y desarrollo

--- config/config.js
+++ config/config.js
@@ -1,4 +1,4 @@
-const PORT = process.env.DB_HOST || 3000
+const PORT = process.env.PORT || 3000
 const DB_HOST = process.env.MONGO_URL || "mongodb://mongo:EgQkfof4gNmcSZO5T28u@containers-us-west-100.railway.app:7011"
 
 module.exports = {

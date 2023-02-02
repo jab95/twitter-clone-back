8a830bfb04f3cb214fe8ff6d88f4f0ec138f6c93
jose <josejab95@gmail.com>
2023-02-02 14:05:11 +0100

    cambia url mongo

--- config/config.js
+++ config/config.js
@@ -1,6 +1,5 @@
 const PORT = process.env.PORT || 3000
-const DB_HOST = process.env.MONGO_URL || "mongodb+srv://jayala:lol123..@cluster0.akjbboy.mongodb.net/?retryWrites=true&w=majority"
-
+const DB_HOST = process.env.MONGO_URL
 module.exports = {
     PORT: PORT,
     DB_HOST: DB_HOST

1dd9a2e32a2465f9fe605fedcc9f58a87cd1937f
jose <josejab95@gmail.com>
2023-03-11 04:11:18 +0100

    cors 2

--- public/javascript/appInit.js
+++ public/javascript/appInit.js
@@ -22,6 +22,7 @@ module.exports = (express, app) => {
     app.use(express.json({ limit: '50mb' }))
     app.use(express.urlencoded({ extended: true }));
 
+
     app.use("/api/usuario", usuarioRoutes)
     app.use("/api/tweet", tweetRoutes)
 

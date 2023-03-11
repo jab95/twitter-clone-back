7eade90f7edeafb53aef1b726483f6794bad4b8a
jose <josejab95@gmail.com>
2023-03-11 03:19:45 +0100

    prueba cors

--- public/javascript/appInit.js
+++ public/javascript/appInit.js
@@ -7,10 +7,14 @@ const path = require("path")
 
 module.exports = (express, app) => {
 
+    var corsOptions = {
+        origin: '*',
+        optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
+    }
 
     app.use(helmet())
     // todo esto siempre antes que las rutas
-    app.use(cors())
+    app.use(cors(corsOptions))
     // app.use(cors())
     app.use(bodyParser.json())
     app.use(express.json({ limit: '50mb' }))

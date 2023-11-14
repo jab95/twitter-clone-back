51a11359a99efab0486fc6c1aa7e7077cb814e69
jose <josejab95@gmail.com>
2023-11-14 17:44:11 +0100

    añade logs usuario

--- routes/usuario.js
+++ routes/usuario.js
@@ -9,7 +9,8 @@ var storageProfile = multer.diskStorage({
         cb(null, "images/profiles")
     },
     filename: (req, file, cb) => {
-        const fileName = file.originalname
+        const regex = /\?.*$/;
+        const fileName = file.originalname.replace(regex, "")
         cb(null, fileName)
     }
 })
@@ -19,7 +20,8 @@ var storageHeader = multer.diskStorage({
         cb(null, "images/headers")
     },
     filename: (req, file, cb) => {
-        const fileName = file.originalname
+        const regex = /\?.*$/;
+        const fileName = file.originalname.replace(regex, "")
         cb(null, fileName)
     }
 })
@@ -35,64 +37,93 @@ const fileFilter = (req, file, cb) => {
 const uploadProfiles = multer({ storage: storageProfile, fileFilter: fileFilter })
 const uploadHeaders = multer({ storage: storageHeader, fileFilter: fileFilter })
 
-router.get("/get", async (req, res) => {
+router.get("/get", async (req, res, next) => {
 
     let usuario = await userManager.getUser(req.query.user, req.query.pass);
     res.status(200).json(usuario)
+    next()
 })
 
-router.get("/getByName", async (req, res) => {
+router.get("/getByName", async (req, res, next) => {
 
     let usuario = await userManager.getUserByName(req.query.user);
 
     res.status(200).json(usuario)
+    next()
+
+
 })
 
-router.get("/getFiltersByName", async (req, res) => {
+router.get("/getFiltersByName", async (req, res, next) => {
 
     let usuario = await userManager.getFiltersByName(req.query.user);
     res.status(200).json(usuario)
+    next()
+
 })
 
 router.post("/add", async (req, res, next) => {
 
     let usuario = await userManager.createUser(req.body.user, req.body.pass, req.body.fotoPerfil, req.body.fotoCabecera);
     res.status(200).json(usuario)
+    next()
+
 })
 
 router.put("/changeUsername", async (req, res, next) => {
 
     let usuario = await userManager.changeUsername(req.body.oldValue, req.body.newValue);
     res.status(200).json(usuario)
+    next()
+
 })
+
+router.put("/changePassword", async (req, res, next) => {
+
+    let usuario = await userManager.changePassword(req.body.user, req.body.newValue);
+    res.status(200).json(usuario)
+    next()
+
+})
+
+
 
 router.put("/changeDescription", async (req, res, next) => {
 
     let usuario = await userManager.changeDescription(req.body.user, req.body.descripcion);
     res.status(200).json(usuario)
+    next()
+
 })
 
 router.put("/changeProfile", async (req, res, next) => {
-
-    let usuario = await userManager.changeProfile(req.body.user, req.body.newProfile);
+    let usuario = await userManager.changeProfile(req.body.user, req.body.newProfile.replace("\?.*$"));
     res.status(200).json(usuario)
+    next()
+
 })
 
 router.put("/changeHeader", async (req, res, next) => {
 
-    let usuario = await userManager.changeHeader(req.body.user, req.body.newHeader);
+    let usuario = await userManager.changeHeader(req.body.user, req.body.newHeader.replace("\?.*$"));
     res.status(200).json(usuario)
+    next()
+
 })
 
 
 router.post("/addImageProfile", uploadProfiles.single("image"), async (req, res, next) => {
 
     res.status(200).json("Imagen adjuntada")
+    next()
+
 })
 
 router.post("/addImageHeader", uploadHeaders.single("image"), async (req, res, next) => {
 
     res.status(200).json("Imagen adjuntada")
+    next()
+
 })
 
 module.exports = router
